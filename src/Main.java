220334fe0c2193bce3c5a14c5e07a98697d7e02a
ThisguynamedSAM <Sam.d.colon@gmail.com>
2013-10-03 17:23:24 -0400

    Added loop that will time sorts

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
-import java.lang.Object;
-
 public class Main{
 
-    static int[] a = new int[1000];
-    static int[] b = new int[1000];
+    private static int[] a = new int[1000000];
+    private static int[] b = new int[1000000];
+    private static int[] time = new int[10]; // will hold 10 different timeframes that sorts will run
+
 	public static void main(String[] args) {
+        long start, end;
 
         Temp.fillRandom(a); // calls fill rand array and passes along a
         Temp.copyArray(a, b);
-        Temp.printArray(b);
-        System.out.println("This is array b before sorting");
-        Temp.shellSort(b);
-        System.out.println("\nThis is array b after sorting");
-        Temp.printArray(b);
-     // Temp.printArray(a);  // print array receives a and prints all elements
 
-        /*
-                       To do
-               add all sorting arrays to AM class
-               create benchmark arrays
-               print results to .csv files
-         */
+        for (int i = 0; i < 10; i++) {
+            start = System.nanoTime(); // assigns start to current time
+
+            Temp.shellSort(b);
+
+            end = System.nanoTime(); // marks current time for end
+
+            time [i] = (int) (long) (timeElapsed(start, end)); // converts to int to be stored in array
+            Temp.copyArray(a, b); // will recopy the array after each iteration
+        }
+
+        System.out.println("10 shell sorts were run, and took the follow times in nanoseconds");
+        Temp.printArray(time);
     }
+
+    private static long timeElapsed(long start, long end) { return end - start; }
 }
