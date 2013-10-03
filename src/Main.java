b9244194e8577c5b35a340f74e1bf86e9d614248
ThisguynamedSAM <Sam.d.colon@gmail.com>
2013-10-03 15:15:04 -0400

    Made a few changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main{
 
+    static int[] a = new int[1000];
+
 	public static void main(String[] args) {
-		System.out.println("My name is Sam!");
-		// create a new print statement with your name
-		System.out.println("Hello Sam, this is Eduardo")
-	}
+
+        Temp.fillRandom(a); // calls fill rand array and passes along a
+        Temp.printArray(a); // print array receives a and prints all elements
+
+        /*
+                       To do
+               add all sorting arrays to AM class
+               create benchmark arrays
+               print results to .csv files
+         */
+    }
 }
