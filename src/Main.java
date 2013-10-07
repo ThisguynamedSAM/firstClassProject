e0dce8d8132c78b1d0d83f960d935b115ede219f
ThisguynamedSAM <Sam.d.colon@gmail.com>
2013-10-07 14:17:04 -0400

    Add working csv writing method

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,35 @@
-public class Main{
+import java.lang.String;
 
-    private static int[] a = new int[1000000];
-    private static int[] b = new int[1000000];
-    private static int[] time = new int[10]; // will hold 10 different timeframes that sorts will run
+public class Main {
 
-	public static void main(String[] args) {
+    private static int[] a = new int[10000];
+    private static int[] b = new int[10000];
+    private static int[] time = new int[10]; // hold one time (in nanoseconds) for each iteration
+
+
+    private static long timeElapsed(long start, long end) {
+
+        return end - start;
+    } // returns elapsed time
+
+    /*
+    *   create a new method to loop through all sorts
+    *   for each sort write times to csv file
+    * */
+    public static void main(String[] args) {
+
         long start, end;
 
         Temp.fillRandom(a); // calls fill rand array and passes along a
-        Temp.copyArray(a, b);
-
         for (int i = 0; i < 10; i++) {
-            start = System.nanoTime(); // assigns start to current time
-
+            Temp.copyArray(a, b); // copies original array a with each iteration
+            start = System.nanoTime(); // start timer
             Temp.shellSort(b);
-
-            end = System.nanoTime(); // marks current time for end
-
-            time [i] = (int) (long) (timeElapsed(start, end)); // converts to int to be stored in array
-            Temp.copyArray(a, b); // will recopy the array after each iteration
+            // AM.selSort(b);
+            end = System.nanoTime(); // end timer
+            time[i] = (int) (long) (timeElapsed(start, end)); // converts long to int
         }
-
-        System.out.println("10 shell sorts were run, and took the follow times in nanoseconds");
-        Temp.printArray(time);
-    }
-
-    private static long timeElapsed(long start, long end) { return end - start; }
+                               // change filepath to the folder location on your computer
+        Temp.generateCsvFile("/Users/Samson/Documents/code/java/firstClassProject/sortTimes.csv", time, "Shell sort");
+    }                                                                            // change string for each function call
 }
--- src/Temp.java
+++ src/Temp.java
@@ -1,10 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 public class Temp {
 
-    public static void fillRandom(int a[]){
+    public static void fillRandom(int a[]) {
         // precondition: array a is an empty array of 1000
-        // this method will call randum number generator for each array element
+        // this method will call rand number generator for each array element
         // array will now be filled with random numbers from 0 to 1000
 
         for (int i = 0; i < a.length; i++)
@@ -12,43 +14,55 @@ public class Temp {
     }
 
     public static int randInt() {
+        // generates a random number from 0 - 1000
         Random rand = new Random();
         return rand.nextInt(1000);
     }
 
     public static void copyArray(int a[], int b[]) {
         // copies each element of array a to array b
-        for(int i = 0; i < a.length; i++)
+        for (int i = 0; i < a.length; i++)
             b[i] = a[i];
     }
 
-    public static void printArray(int d[]) {
-        for (int i = 0; i < d.length; i++) {
-            // prints 5 ints per line
-            if (i % 5 == 0) System.out.print("\n");
+    public static void shellSort(int[] b) {
 
-            System.out.print(d[i] + " ");
-        }
-    }
-
-    public static void shellSort(int[] b){
         int h = 1;
-        while (h < b.length) h = 3*h + 1;
-        while (h > 0){
-            h = h/3;
-            for (int k = 0; k < h; k++){
-                for (int i = h+k; i < b.length; i+=h){
+        while (h < b.length) h = 3 * h + 1;
+        while (h > 0) {
+            h = h / 3;
+            for (int k = 0; k < h; k++) {
+                for (int i = h + k; i < b.length; i += h) {
                     int key = b[i];
-                    int j = i-h;
-                    while (j>=0 && b[j] > key){
-                        b[j+h] = b[j];
-                        j-=h;
+                    int j = i - h;
+                    while (j >= 0 && b[j] > key) {
+                        b[j + h] = b[j];
+                        j -= h;
                     }
-                    b[j+h] = key;
+                    b[j + h] = key;
                     //-> invariant: array[0,h,2*h..j] is sorted
                 }
             }
             //->invariant: each h-sub-array is sorted
+        }
+    }
+
+    public static void generateCsvFile(String sFileName, int d[], String sortName) {
+        // method is called with file path, array containing run times, and a string with the name of the sort
+        // after this method is called a new line will be added to the .csv file containing the sort name and all
+        // run times
+        try {
+            FileWriter writer = new FileWriter(sFileName, true); // where true means add to end of file
+            writer.append("\n" + sortName + ",");
+
+            for (int i = 0; i < d.length; i++) {
+                writer.append(d[i] + ",");
+            }
+            writer.append("Nano seconds");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
